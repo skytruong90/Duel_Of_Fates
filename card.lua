b9ede23bd8e07e2bf14aecb83c273e43886ae9fb
Robert Carroll <robert.carroll.eportfolio@gmail.com>
2024-03-02 06:57:26 -0800

    pausing to go eat breakfast

--- card.lua
+++ card.lua
@@ -1,20 +1,65 @@
-Card = {
-}
+CARD_WIDTH = 70
+CARD_HEIGHT = 100
 
-function Card:new(name, attack, defense, ... )
-    local card = {}
-    card.__index = Card
-    --input arguments
-    card.name = name
-    card.attack = attack
-    card.defense = defense
+utils = require("utils")
 
-    for i, keyword in ipairs({...}) do
-        if i == 1 then
-            card.cost = keyword
-        end
-    end
+Card = {}
+Card.__index = Card
+Card.rearImage = love.graphics.newImage("assets/card back black.png")
+Card.backgroundImage = love.graphics.newImage("assets/parchment.jpg")
+Card.cost = 0
 
+function Card:loadBackground()
+   love.graphics.setColor(1,1,1)
+   utils.drawSizedImage(self.backgroundImage, 0, 0, 0, CARD_WIDTH, CARD_HEIGHT)
+end
+
+function Card:loadName()
+    love.graphics.setColor(0,0,0)
+    love.graphics.print(self.name, 20, 40)
+end
+
+function Card:loadCost()
+    love.graphics.setColor(0,0,1)
+    love.graphics.print(self.cost, 0, 0)
+end
+
+function Card:loadAttack()
+    love.graphics.setColor(1,1,0)
+    love.graphics.print(self.attack or "", 62, 84)
+end
+
+function Card:loadHealth()
+    love.graphics.setColor(1,0,0)
+    love.graphics.print(self.health or "", 0, 84)
+end
+
+function Card:loadCanvas()
+    local cardCanvas = love.graphics.newCanvas()
+    cardCanvas:renderTo(function() 
+        self:loadBackground()
+        self:loadCost()
+        self:loadAttack()
+        self:loadHealth()
+        self:loadName()
+    end)
+    self.canvas = cardCanvas
+end
+
+
+
+function Card:newUnit(name, cost, attack, health, keywords, artPath)
+    local card = {
+        name = name,
+        cost = cost,
+        attack = attack,
+        health = health,
+        keywords = keywords,
+        artPath = artPath,
+    }
+    
+    setmetatable(card, self)
     return card
 end
+
 return Card
--- main.lua
+++ main.lua
@@ -3,68 +3,57 @@ love = love -- quick hack to stop lsp from showing errors
 Card = require("card")
 Player = require("player")
 
+DECK_SIZE = 50
+
+function love.graphics.resizedImageFromData(imageData, newWidth, newHeight) 
+    local oldWidth, oldHeight = imageData.getDimensions()
+    local newImageData = love.image.newImageData(newWidth, newHeight, "rgba8", imageData)
+    return love.graphics.newImage(newImageData)
+end
+
 function love.load()
-    CARD_WIDTH = 50
-    CARD_HEIGHT = 100
-end
+    exampleCard = Card:newUnit("wolf",1,2,1)
+    exampleCard:loadCanvas()
 
+    math.randomseed(os.time())
+    -- Create players with different characters
+    player1 = Player:new("Student") -- Character 1
+    player2 = Player:new("The Emperor") -- Character 2
 
-
--- Create players with different characters
-player1 = Player:new("Student") -- Character 1
-player2 = Player:new("The Emperor") -- Character 2
-
--- Create decks
-for i = 1, 50 do
-    local attack = math.random(1, 10)
-    local defense = math.random(1, 10)
-    player1:addCardToDeck(Card:new("Card " .. i, attack, defense))
-    player2:addCardToDeck(Card:new("Card " .. i, attack, defense))
-end
-
--- Players draw cards and battle
-local battleResult = "Press space to start the game"
-local cardPlayer1, cardPlayer2
-
-function love.keypressed(key)
-    if key == "space" then
-        cardPlayer1 = player1:drawCard()
-        cardPlayer2 = player2:drawCard()
-
-        battleResult = cardPlayer1.name .. " vs " .. cardPlayer2.name
-
-        if cardPlayer1.attack > cardPlayer2.defense then
-            battleResult = battleResult .. "\n" .. player1.name .. " wins!"
-        elseif cardPlayer1.attack < cardPlayer2.defense then
-            battleResult = battleResult .. "\n" .. player2.name .. " wins!"
-        else
-            battleResult = battleResult .. "\n" .. "It's a draw!"
-        end
+    all_cards = require("cards")
+    for i = 1, DECK_SIZE do
+        cardIndex = math.random(1, #all_cards)
+        player1:addCardToDeck(cards[cardIndex])
+        cardIndex = math.random(1, #all_cards)
+        player2:addCardToDeck(cards[cardIndex])
     end
 end
+
+
+function love.mousepressed(x, y, button, istouch, presses)
+    print(x, y)
+    if utils.rectContainsPoint(5,5, 70, 100, x, y) then
+        player1:drawCard()
+    end
+end
+
+
+function love.update(dt)
+    --print("X:   " .. love.mouse.getX() .. "   Y:   " .. love.mouse.getY())
+end
+
+
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1) -- Set color to white for the text
-    love.graphics.print(battleResult, 10, 10)
+    --love.graphics.setColor(1,1,1)
+    --love.graphics.draw(exampleCard.canvas, 0, 0, 0, 1)
 
-    if cardPlayer1 then
-        love.graphics.setColor(0, 0, 1) -- Set color to blue for card display
-        love.graphics.print(player1.name .. "'s Card: " .. cardPlayer1.name, 10, 30)
-        love.graphics.print("Attack: " .. cardPlayer1.attack, 10, 50)
-        love.graphics.print("Defense: " .. cardPlayer1.defense, 10, 70)
-
-        love.graphics.setColor(1, 0, 0) -- Set color to red for card display
-        love.graphics.print(player2.name .. "'s Card: " .. cardPlayer2.name, 10, 100)
-        love.graphics.print("Attack: " .. cardPlayer2.attack, 10, 120)
-        love.graphics.print("Defense: " .. cardPlayer2.defense, 10, 140)
+    --draw decks
+    love.graphics.setColor(1,1,1)
+    utils.drawSizedImage(Card.rearImage, 5, 395, 0, 70, 100)
+    love.graphics.print("remaining cards: ".. #player1.deck, 5, 495)
+    utils.drawSizedImage(Card.rearImage, 5, 5, 0, 70, 100)
+    love.graphics.print("remaining cards: ".. #player2.deck, 5, 105)
 
 
-        -- Add graphical elements (e.g., card images, character avatars) here
-        -- You can load images using love.graphics.newImage() and display them
-        -- alongside the card information.
-        -- For example:
-        -- local cardImage1 = love.graphics.newImage("card1.png")
-        -- love.graphics.draw(cardImage1, 200, 30)
-        -- ...
-    end
 end
